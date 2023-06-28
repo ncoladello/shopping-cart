0fe5a19f61b4f84e12ba4478a50e21c43c864e39
Natanael Coladello <ncoladello@gmail.com>
2023-06-28 23:13:02 +0200

    some improvements on SearchBar component

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BsSearch } from 'react-icons/bs';
 
-import { BsSearch} from 'react-icons/bs';
 import './SearchBar.css';
 
 function SearchBar() {
+
+  const [searchValue, setSearchValue] = useState('');
+
   return (
     <form className="search-bar">
-      <input type="search" placeholder="Buscar produtos" className="search__input" />
+      <input type="search"
+        value={searchValue} 
+        placeholder="Buscar produtos" 
+        className="search__input"
+        onChange={({target}) =>setSearchValue(target.value)}       
+        required />
+
       <button type="submit" className="search__button">
         <BsSearch />
       </button>
