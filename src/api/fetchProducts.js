9599305f87feadd38ef015efc4ba0263ae153c02
Natanael Coladello <ncoladello@gmail.com>
2023-06-30 08:41:56 +0200

    fix query not declared

--- src/api/fetchProducts.js
+++ src/api/fetchProducts.js
@@ -1,7 +1,5 @@
-const fetchProducts = async () => {
-  const response = await fetch(
-    `https://api.mercadolibre.com/sites/MLB/search?q=${query}`
-  );
+const fetchProducts = async (query) => {
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
   const data = await response.json();
 
   return data.results;
