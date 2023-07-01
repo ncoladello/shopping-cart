fdf0f7c835435a687e9d6049a148d9f2b11b8d80
Natanael Coladello <ncoladello@gmail.com>
2023-07-01 17:37:30 +0200

    Created n stylized a Loading and added it on Products.jsx

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -3,15 +3,16 @@ import React, { useState, useEffect } from 'react';
 import './Products.css';
 import fetchProducts from '../../api/fetchProducts';
 import ProductCard from '../ProductCard/ProductCard';
+import Loading from '../Loading/Loading';
 
 function Products() {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-
     fetchProducts('iphone').then((response) => {
-
       setProducts(response);
+      setLoading(false);
 
     });
   }, []);
@@ -20,19 +21,14 @@ function Products() {
 
   return (
 
-
-
-    <section className="products container">
-      
+    (loading ? <Loading /> : <section className="products container">
       {
         products.map((product) => <ProductCard key={product.id} data={product} />)
-      }
+      }</section>)
 
-    
 
-      
-      
-    </section>
+
+
 
 
   );
