bd9d57ca3f7ffe63c92c34c3715a57f6e85338be
Natanael Coladello <ncoladello@gmail.com>
2023-07-04 00:25:01 +0200

    created the function of useContext

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,13 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React, {  useEffect, useContext } from 'react';
 
 import './Products.css';
 import fetchProducts from '../../api/fetchProducts';
 import ProductCard from '../ProductCard/ProductCard';
 import Loading from '../Loading/Loading';
+import AppContext from '../../context/AppContext';
 
 function Products() {
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(true);
+
+  const {products, setProducts, loading, setLoading} = useContext(AppContext);
+  
 
   useEffect(() => {
     fetchProducts('iphone').then((response) => {
@@ -17,20 +19,13 @@ function Products() {
     });
   }, []);
 
-  console.log(products);
-
   return (
-
-    (loading ? <Loading /> : <section className="products container">
-      {
-        products.map((product) => <ProductCard key={product.id} data={product} />)
-      }</section>)
-
-
-
-
-
-
+    (loading && <Loading /> ) || (
+      <section className="products container">
+        {products.map((product) => <ProductCard key={product.id} data={product} />)}
+      </section>
+    )
+      
   );
 }
 
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,28 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { BsSearch } from 'react-icons/bs';
-
 
 import './SearchBar.css';
 import fetchProducts from '../../api/fetchProducts';
+import AppContext from '../../context/AppContext';
 
 
 function SearchBar() {
 
+  const {setProducts, setLoading} = useContext(AppContext);
   const [searchValue, setSearchValue] = useState('');
-
+  
   const handleSearch = async (event) => {
     event.preventDefault();
+    setLoading(true);
 
-    const products = await fetchProducts();
-    console.log(products);
+    const products = await fetchProducts(searchValue);
+
+    setProducts(products);
+    setLoading(false);
     setSearchValue('');
-
-
+    
   };
 
   return (
     <form className="search-bar" onSubmit={handleSearch}>
-      <input type="search"
+      {name}
+      <input
+        type="search"
         value={searchValue}
         placeholder="Buscar produtos"
         className="search__input"
