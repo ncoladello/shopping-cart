0d430f4890fab6d9ccb23f9c0caf702245a60a1c
Natanael Coladello <ncoladello@gmail.com>
2023-07-04 18:17:17 +0200

    moved

---
+++ src/components/CartItem/CartItem.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import {BsCartDashFill} from 'react-icons/bs';
+import propTypes from 'prop-types';
+
+import './CartItem.css';
+import formatCurrency from '../../utils/formatCurrency';
+
+
+function CartItem({data}){
+
+
+  const {thumbnail, title, price} = data;
+  return(
+    <section className="cart-item">
+      <img src={thumbnail}
+        alt="product image"
+        className="cart-item-image" 
+      /> 
+      <div className="cart-item-content">
+        <h3 className="cart-item-title">
+          {title}
+        </h3>
+        <h3 className="cart-item-price">{formatCurrency(price, 'BRL')}</h3>
+        <button type="button"
+          className="button__remove-item">
+
+          <BsCartDashFill/>
+        </button>
+      </div>
+
+    </section>
+  );
+}
+
+
+export default CartItem;
+
+CartItem.propTypes = {
+  data : propTypes.object}.isRequired;
+
