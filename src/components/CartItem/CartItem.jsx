f9f5bcc1d3ad4457f2c332c4093574078eda926b
Natanael Coladello <ncoladello@gmail.com>
2023-07-04 18:25:29 +0200

    add: button__remove-item behavior

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -1,30 +1,43 @@
-import React from 'react';
-import {BsCartDashFill} from 'react-icons/bs';
+import React, { useContext } from 'react';
+import { BsCartDashFill } from 'react-icons/bs';
 import propTypes from 'prop-types';
 
 import './CartItem.css';
 import formatCurrency from '../../utils/formatCurrency';
+import AppContext from '../../context/AppContext';
 
 
-function CartItem({data}){
+function CartItem({ data }) {
+
+  const{cartItems, setCartItems} =useContext(AppContext);
+  const { id, thumbnail, title, price } = data;
+
+  const handleRemoveItem =() =>{
+
+    const updatedItems = cartItems.filter((item)=> item.id != id);
+    setCartItems(updatedItems);
 
 
-  const {thumbnail, title, price} = data;
-  return(
+  };
+  
+  return (
     <section className="cart-item">
       <img src={thumbnail}
         alt="product image"
-        className="cart-item-image" 
-      /> 
+        className="cart-item-image"
+      />
       <div className="cart-item-content">
         <h3 className="cart-item-title">
           {title}
         </h3>
         <h3 className="cart-item-price">{formatCurrency(price, 'BRL')}</h3>
-        <button type="button"
-          className="button__remove-item">
+        <button 
+          type="button"
+          className="button__remove-item"
+          onClick={handleRemoveItem}
+        >
 
-          <BsCartDashFill/>
+          <BsCartDashFill />
         </button>
       </div>
 
@@ -36,5 +49,6 @@ function CartItem({data}){
 export default CartItem;
 
 CartItem.propTypes = {
-  data : propTypes.object}.isRequired;
+  data: propTypes.object
+}.isRequired;
 
